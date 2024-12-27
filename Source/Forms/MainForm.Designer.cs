97960cff44a958f11f7c39c339d08d2b11e96951
fred <domaine75@gmail.com>
2024-12-27 15:51:30 +0100

    déplacement des controles de test connextion vers tab control 1

--- Source/Forms/MainForm.Designer.cs
+++ Source/Forms/MainForm.Designer.cs
@@ -28,62 +28,145 @@ namespace DatabaseMigration
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabConnexion = new System.Windows.Forms.TabPage();
-            this.btnTestConnections = new System.Windows.Forms.Button();
             
-            // 
-            // tabControl
-            // 
+            // Groupes Oracle et PostgreSQL
+            this.grpOracle = new System.Windows.Forms.GroupBox();
+            this.grpPostgres = new System.Windows.Forms.GroupBox();
+            
+            // Contrôles Oracle
+            this.txtOracleHost = new System.Windows.Forms.TextBox();
+            this.txtOraclePort = new System.Windows.Forms.TextBox();
+            this.txtOracleService = new System.Windows.Forms.TextBox();
+            this.txtOracleUser = new System.Windows.Forms.TextBox();
+            this.txtOraclePassword = new System.Windows.Forms.TextBox();
+            this.btnTestOracle = new System.Windows.Forms.Button();
+            
+            // Contrôles PostgreSQL
+            this.txtPgHost = new System.Windows.Forms.TextBox();
+            this.txtPgPort = new System.Windows.Forms.TextBox();
+            this.txtPgDatabase = new System.Windows.Forms.TextBox();
+            this.txtPgUser = new System.Windows.Forms.TextBox();
+            this.txtPgPassword = new System.Windows.Forms.TextBox();
+            this.btnTestPg = new System.Windows.Forms.Button();
+            
+            // Bouton Sauvegarder
+            this.btnSave = new System.Windows.Forms.Button();
+
+            // Configuration TabControl
             this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl.Location = new System.Drawing.Point(0, 0);
             this.tabControl.Name = "tabControl";
-            this.tabControl.SelectedIndex = 0;
             this.tabControl.Size = new System.Drawing.Size(800, 600);
             this.tabControl.TabIndex = 0;
             
-            // 
-            // tabConnexion
-            // 
+            // Configuration TabPage Connexion
             this.tabConnexion.Location = new System.Drawing.Point(4, 22);
             this.tabConnexion.Name = "tabConnexion";
             this.tabConnexion.Padding = new System.Windows.Forms.Padding(10);
             this.tabConnexion.Size = new System.Drawing.Size(792, 574);
-            this.tabConnexion.TabIndex = 0;
             this.tabConnexion.Text = "Connexion";
             this.tabConnexion.UseVisualStyleBackColor = true;
-            
-            // 
-            // btnTestConnections
-            // 
-            this.btnTestConnections.Location = new System.Drawing.Point(20, 20);
-            this.btnTestConnections.Name = "btnTestConnections";
-            this.btnTestConnections.Size = new System.Drawing.Size(150, 30);
-            this.btnTestConnections.TabIndex = 0;
-            this.btnTestConnections.Text = "Tester les connexions";
-            this.btnTestConnections.UseVisualStyleBackColor = true;
-            this.btnTestConnections.Click += new System.EventHandler(this.BtnTestConnections_Click);
-            
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 600);
+
+            // Configuration GroupBox Oracle
+            this.grpOracle.Text = "Connexion Oracle";
+            this.grpOracle.Location = new System.Drawing.Point(10, 10);
+            this.grpOracle.Size = new System.Drawing.Size(280, 300);
+
+            // Configuration GroupBox PostgreSQL
+            this.grpPostgres.Text = "Connexion PostgreSQL";
+            this.grpPostgres.Location = new System.Drawing.Point(300, 10);
+            this.grpPostgres.Size = new System.Drawing.Size(280, 300);
+
+            // Configuration des contrôles Oracle
+            CreateLabelAndTextBox("Hôte:", this.txtOracleHost, 20, 30, this.grpOracle);
+            CreateLabelAndTextBox("Port:", this.txtOraclePort, 20, 80, this.grpOracle);
+            CreateLabelAndTextBox("Service:", this.txtOracleService, 20, 130, this.grpOracle);
+            CreateLabelAndTextBox("Utilisateur:", this.txtOracleUser, 20, 180, this.grpOracle);
+            CreateLabelAndTextBox("Mot de passe:", this.txtOraclePassword, 20, 230, this.grpOracle);
+            this.txtOraclePassword.PasswordChar = '*';
+            this.txtOraclePort.Text = "1521";
+
+            this.btnTestOracle.Location = new System.Drawing.Point(90, 260);
+            this.btnTestOracle.Size = new System.Drawing.Size(100, 30);
+            this.btnTestOracle.Text = "Tester Oracle";
+            this.btnTestOracle.Click += new System.EventHandler(this.BtnTestOracle_Click);
+
+            // Configuration des contrôles PostgreSQL
+            CreateLabelAndTextBox("Hôte:", this.txtPgHost, 20, 30, this.grpPostgres);
+            CreateLabelAndTextBox("Port:", this.txtPgPort, 20, 80, this.grpPostgres);
+            CreateLabelAndTextBox("Base:", this.txtPgDatabase, 20, 130, this.grpPostgres);
+            CreateLabelAndTextBox("Utilisateur:", this.txtPgUser, 20, 180, this.grpPostgres);
+            CreateLabelAndTextBox("Mot de passe:", this.txtPgPassword, 20, 230, this.grpPostgres);
+            this.txtPgPassword.PasswordChar = '*';
+
+            this.btnTestPg.Location = new System.Drawing.Point(90, 260);
+            this.btnTestPg.Size = new System.Drawing.Size(100, 30);
+            this.btnTestPg.Text = "Tester PostgreSQL";
+            this.btnTestPg.Click += new System.EventHandler(this.BtnTestPg_Click);
+
+            // Configuration du bouton Sauvegarder
+            this.btnSave.Location = new System.Drawing.Point(250, 320);
+            this.btnSave.Size = new System.Drawing.Size(100, 30);
+            this.btnSave.Text = "Sauvegarder";
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+
+            // Ajout des contrôles aux groupes
+            this.grpOracle.Controls.Add(this.btnTestOracle);
+            this.grpPostgres.Controls.Add(this.btnTestPg);
+
+            // Ajout des groupes et du bouton sauvegarder à l'onglet
+            this.tabConnexion.Controls.AddRange(new System.Windows.Forms.Control[] { 
+                this.grpOracle, 
+                this.grpPostgres, 
+                this.btnSave 
+            });
+
+            // Configuration du formulaire principal
+            this.tabControl.Controls.Add(this.tabConnexion);
             this.Controls.Add(this.tabControl);
+            
+            this.ClientSize = new System.Drawing.Size(800, 600);
             this.Name = "MainForm";
             this.Text = "Migration Oracle vers PostgreSQL";
             
-            this.tabConnexion.Controls.Add(this.btnTestConnections);
-            this.tabControl.Controls.Add(this.tabConnexion);
             this.ResumeLayout(false);
+        }
+
+        private void CreateLabelAndTextBox(string labelText, System.Windows.Forms.TextBox textBox, int x, int y, System.Windows.Forms.GroupBox parent)
+        {
+            var label = new System.Windows.Forms.Label
+            {
+                Text = labelText,
+                Location = new System.Drawing.Point(x, y),
+                Size = new System.Drawing.Size(80, 20)
+            };
+
+            textBox.Location = new System.Drawing.Point(x + 90, y);
+            textBox.Size = new System.Drawing.Size(150, 20);
+
+            parent.Controls.AddRange(new System.Windows.Forms.Control[] { label, textBox });
         }
 
         #endregion
 
         private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.TabPage tabConnexion;
-        private System.Windows.Forms.Button btnTestConnections;
+        private System.Windows.Forms.GroupBox grpOracle;
+        private System.Windows.Forms.GroupBox grpPostgres;
+        private System.Windows.Forms.TextBox txtOracleHost;
+        private System.Windows.Forms.TextBox txtOraclePort;
+        private System.Windows.Forms.TextBox txtOracleService;
+        private System.Windows.Forms.TextBox txtOracleUser;
+        private System.Windows.Forms.TextBox txtOraclePassword;
+        private System.Windows.Forms.TextBox txtPgHost;
+        private System.Windows.Forms.TextBox txtPgPort;
+        private System.Windows.Forms.TextBox txtPgDatabase;
+        private System.Windows.Forms.TextBox txtPgUser;
+        private System.Windows.Forms.TextBox txtPgPassword;
+        private System.Windows.Forms.Button btnTestOracle;
+        private System.Windows.Forms.Button btnTestPg;
+        private System.Windows.Forms.Button btnSave;
     }
 } 
--- Source/Forms/MainForm.cs
+++ Source/Forms/MainForm.cs
@@ -1,20 +1,188 @@
-using DatabaseMigration.Forms;
 using System;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Npgsql;
+using System.Configuration;
+using DatabaseMigration.Utils;
+using System.IO;
+using System.Text.Json;
 
 namespace DatabaseMigration
 {
-  public partial class MainForm : Form
-  {
-    public MainForm()
+    public partial class MainForm : Form
     {
-      InitializeComponent();
-    }
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadConnectionStrings();
+            LoadCredentials();
+        }
 
-    private void BtnTestConnections_Click(object sender, EventArgs e)
-    {
-      var testForm = new ConnectionTestForm();
-      testForm.ShowDialog();
+        private void LoadConnectionStrings()
+        {
+            try
+            {
+                var oracleConnStr = ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString;
+                var pgConnStr = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
+
+                // Parse Oracle connection string
+                var oracleBuilder = new OracleConnectionStringBuilder(oracleConnStr);
+                txtOracleHost.Text = oracleBuilder.DataSource;
+                txtOracleUser.Text = oracleBuilder.UserID;
+                txtOraclePassword.Text = oracleBuilder.Password;
+
+                // Parse Postgres connection string
+                var pgBuilder = new NpgsqlConnectionStringBuilder(pgConnStr);
+                txtPgHost.Text = pgBuilder.Host;
+                txtPgPort.Text = pgBuilder.Port.ToString();
+                txtPgDatabase.Text = pgBuilder.Database;
+                txtPgUser.Text = pgBuilder.Username;
+                txtPgPassword.Text = pgBuilder.Password;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors du chargement des paramètres: {ex.Message}", 
+                    "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnTestOracle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connStr = BuildOracleConnectionString();
+                using (var conn = new OracleConnection(connStr))
+                {
+                    conn.Open();
+                    MessageBox.Show("Connexion Oracle réussie!", "Succès", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur de connexion Oracle: {ex.Message}", 
+                    "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnTestPg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connStr = BuildPostgresConnectionString();
+                using (var conn = new NpgsqlConnection(connStr))
+                {
+                    conn.Open();
+                    MessageBox.Show("Connexion PostgreSQL réussie!", "Succès", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur de connexion PostgreSQL: {ex.Message}", 
+                    "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveConnectionStrings();
+                SaveCredentials();
+                MessageBox.Show("Paramètres sauvegardés avec succès!", "Succès", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la sauvegarde: {ex.Message}", 
+                    "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string BuildOracleConnectionString()
+        {
+            return $"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={txtOracleHost.Text})(PORT={txtOraclePort.Text}))" +
+                   $"(CONNECT_DATA=(SERVICE_NAME={txtOracleService.Text})));User Id={txtOracleUser.Text};Password={txtOraclePassword.Text}";
+        }
+
+        private string BuildPostgresConnectionString()
+        {
+            return $"Host={txtPgHost.Text};Port={txtPgPort.Text};Database={txtPgDatabase.Text};" +
+                   $"Username={txtPgUser.Text};Password={txtPgPassword.Text}";
+        }
+
+        private void SaveConnectionStrings()
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings["OracleConnection"].ConnectionString = BuildOracleConnectionString();
+            config.ConnectionStrings.ConnectionStrings["PostgresConnection"].ConnectionString = BuildPostgresConnectionString();
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+
+        private void SaveCredentials()
+        {
+            var credentials = new
+            {
+                Oracle = new
+                {
+                    Host = txtOracleHost.Text,
+                    Port = txtOraclePort.Text,
+                    Service = txtOracleService.Text,
+                    User = txtOracleUser.Text,
+                    Password = txtOraclePassword.Text
+                },
+                Postgres = new
+                {
+                    Host = txtPgHost.Text,
+                    Port = txtPgPort.Text,
+                    Database = txtPgDatabase.Text,
+                    User = txtPgUser.Text,
+                    Password = txtPgPassword.Text
+                }
+            };
+
+            string json = JsonSerializer.Serialize(credentials);
+            string encrypted = SecurityHelper.Encrypt(json);
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            string credentialsPath = Path.Combine(appPath, "id.txt");
+            File.WriteAllText(credentialsPath, encrypted);
+        }
+
+        private void LoadCredentials()
+        {
+            try
+            {
+                string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+                string credentialsPath = Path.Combine(appPath, "id.txt");
+
+                if (File.Exists(credentialsPath))
+                {
+                    string encrypted = File.ReadAllText(credentialsPath);
+                    string json = SecurityHelper.Decrypt(encrypted);
+                    var credentials = JsonSerializer.Deserialize<dynamic>(json);
+
+                    // Oracle
+                    txtOracleHost.Text = credentials.Oracle.Host.GetString();
+                    txtOraclePort.Text = credentials.Oracle.Port.GetString();
+                    txtOracleService.Text = credentials.Oracle.Service.GetString();
+                    txtOracleUser.Text = credentials.Oracle.User.GetString();
+                    txtOraclePassword.Text = credentials.Oracle.Password.GetString();
+
+                    // PostgreSQL
+                    txtPgHost.Text = credentials.Postgres.Host.GetString();
+                    txtPgPort.Text = credentials.Postgres.Port.GetString();
+                    txtPgDatabase.Text = credentials.Postgres.Database.GetString();
+                    txtPgUser.Text = credentials.Postgres.User.GetString();
+                    txtPgPassword.Text = credentials.Postgres.Password.GetString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors du chargement des identifiants: {ex.Message}", 
+                    "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
-  }
 }
