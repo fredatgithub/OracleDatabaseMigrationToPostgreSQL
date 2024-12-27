2b8be9f3f8b1761d6410294bb8426f9a1c0327a6
fred <domaine75@gmail.com>
2024-12-27 15:22:41 +0100

    create a tabcontrol

--- Source/Forms/MainForm.cs
+++ Source/Forms/MainForm.cs
@@ -9,44 +9,52 @@ namespace DatabaseMigration
 {
   public partial class MainForm : Form
   {
+    private TabControl tabControl;
+    private TabPage tabConnexion;
+    
     public MainForm()
     {
       InitializeComponent();
-    }
-
-    private void BtnConnect_Click(object sender, EventArgs e)
-    {
-      try
-      {
-        using (var oracleConnection = new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString))
-        {
-          oracleConnection.Open();
-          MessageBox.Show("Connexion à Oracle réussie!");
-        }
-
-        using (var pgConnection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString))
-        {
-          pgConnection.Open();
-          MessageBox.Show("Connexion à PostgreSQL réussie!");
-        }
-      }
-      catch (Exception exception)
-      {
-        MessageBox.Show($"Erreur de connexion: {exception.Message}");
-      }
+      SetupTabControl();
     }
 
     private void InitializeComponent()
     {
+      this.Text = "Migration Oracle vers PostgreSQL";
+      this.Size = new System.Drawing.Size(800, 600);
+
+      // Création du TabControl
+      tabControl = new TabControl
+      {
+        Dock = DockStyle.Fill
+      };
+
+      this.Controls.Add(tabControl);
+    }
+
+    private void SetupTabControl()
+    {
+      // Création de l'onglet Connexion
+      tabConnexion = new TabPage
+      {
+        Text = "Connexion",
+        Padding = new Padding(10)
+      };
+
+      // Ajout du bouton de test de connexion
       Button btnTestConnections = new Button
       {
         Text = "Tester les connexions",
-        Location = new System.Drawing.Point(10, 10),
+        Location = new System.Drawing.Point(20, 20),
         Size = new System.Drawing.Size(150, 30)
       };
       btnTestConnections.Click += BtnTestConnections_Click;
 
-      Controls.Add(btnTestConnections);
+      // Ajout des contrôles à l'onglet
+      tabConnexion.Controls.Add(btnTestConnections);
+
+      // Ajout de l'onglet au TabControl
+      tabControl.TabPages.Add(tabConnexion);
     }
 
     private void BtnTestConnections_Click(object sender, EventArgs e)
